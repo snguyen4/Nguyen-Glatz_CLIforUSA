58c1c54dbdb5a2cb39c23a7abdeecfbde51c4f67
snguyen4 <samuelnguyen91@gmail.com>
2021-11-27 09:59:24 +0100

    Added non-durable consumer goods. Lagging.

--- Nguyen-Glatz_CLIforUSA_Code.R
+++ Nguyen-Glatz_CLIforUSA_Code.R
@@ -37,18 +37,27 @@ IP = ts_fred('IPTB56300S')
 IP = xts(IP[, 3], order.by = as.Date(IP[, 2]))
 print(paste("End of IP", ts_summary(IP)$end, sep = ": "))
 
+#Industrial production: Non-durable consumer goods
+IPNCCONGD = ts_fred('IPNCONGD')
+IPNCCONGD = xts(IPNCCONGD[, 3], order.by = as.Date(IPNCCONGD[, 2]))
+print(paste("End of IPNCCONGD", ts_summary(IPNCCONGD)$end, sep = ": "))
+
 #Plotting Industrial production. We can see that it is not stationary
 ts_ggplot(log(IP))
 
+#Plotting IPNCCONGD
+ts_ggplot(log(IPNCCONGD))
+
 #Plotting month on month growth rates. Roughly similar to first log-differences.
 ts_ggplot(ts_pc(IP))
+ts_ggplot(ts_pc(IPNCCONGD))
 
 # Shorten all series to start in 1980-01-01
 myStart = "1980-01-01"
 
 IP  = ts_span(IP, start = myStart)
 GDP  = ts_span(GDP, start = myStart)
-
+IPNCCONGD = ts_span(IPNCCONGD, start = myStart)
 
 #Augmented Dickey-FUller test. First one clearly non-stationary. Log differences stationary.
 uRootIP = CADFtest(log(IP), max.lag.y = 10, type = "trend", criterion = "BIC")
@@ -57,12 +66,22 @@ summary(uRootIP)
 uRootIPd = CADFtest(ts_diff(log(IP)), max.lag.y = 10, type = "drift", criterion = "BIC")
 summary(uRootIPd)
 
-#Making IP stationary
+uRootIPNCCONGD = CADFtest(log(IPNCCONGD), max.lag.y = 10, type = "trend", criterion = "BIC")
+summary(uRootIPNCCONGD)
+
+uRootIPNCCONGDd = CADFtest(ts_diff(log(IPNCCONGD)), max.lag.y = 10, type = "drift", criterion = "BIC")
+summary(uRootIPNCCONGDd)
+
+#Making series stationary
 dIP = ts_diff(log(IP))
 
 ts_ggplot(dIP)
 
-#Examine lead-lag with CCF. Coincident indicator.
+dIPNCCONGD = ts_diff(log(IPNCCONGD))
+
+ts_ggplot(dIPNCCONGD)
+
+#Examine lead-lag with CCF. Coincident indicator. IP
 dIPq = ts_frequency(dIP, to = "quarter", aggregate= "mean", na.rm = TRUE)
 p <- plotCCF(ts_ts(dIPq), ts_ts(GDP), lag.max = 15)
 p <- ggLayout(p)+ ylab("Cross correlation X(t+s), GDP(t)") +
@@ -76,5 +95,21 @@ p <- plotCCF(ts_ts(resid(ModeldIP)), ts_ts(resid(ModelGDP)), lag.max = 15)
 p <- ggLayout(p)+ ylab("Cross correlation X(t+s), GDP(t)") +
   labs(title = "Pre-whitened Cross-correlation between industrial production and GDP growth", subtitle = "Quarterly")
 p
+
+
+#Examine lead-lag with CCF. Coincident indicator. IP non-durable goods
+dIPNCCONGDq = ts_frequency(dIPNCCONGD, to = "quarter", aggregate= "mean", na.rm = TRUE)
+p <- plotCCF(ts_ts(dIPNCCONGDq), ts_ts(GDP), lag.max = 15)
+p <- ggLayout(p)+ ylab("Cross correlation X(t+s), GDP(t)") +
+  labs(title = "Cross-correlation between industrial production: non-durable goods and GDP growth", subtitle = "Quarterly")
+p
+
+#Pre-whitening data. Lead of one quarter. Lagging one quarter
+ModeldIPNCONGD  <- auto.arima(dIPNCCONGDq, max.p = 5, max.q = 5, ic = c("bic"))
+p <- plotCCF(ts_ts(resid(ModeldIPNCONGD)), ts_ts(resid(ModelGDP)), lag.max = 15)
+p <- ggLayout(p)+ ylab("Cross correlation X(t+s), GDP(t)") +
+  labs(title = "Pre-whitened Cross-correlation between industrial production and GDP growth", subtitle = "Quarterly")
+p
+
 
 
